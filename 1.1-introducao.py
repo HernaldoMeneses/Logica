9a74a2fa22fc56ab98497d49d1ea702a6a0e3441
HernaldoMeneses <hernaldomeneses@gmail.com>
2024-03-03 21:44:21 -0300

    1.1-introducao.py

---
+++ 1.1-introducao.py
@@ -0,0 +1,44 @@
+'''
+    Filosofia
+        Varios campos
+            Logica
+                Atribui-se a Aristóteles (384-322 a.C.) o inicio desse estudo
+'''
+
+'''
+    Lógica
+        raciocínio
+        pensamento
+        certeza proposicional
+        estruturação dos encadeamentos racionais
+        regras do procedimento racional
+        inferências
+        deduções
+        induções
+        (...)
+'''
+
+'''
+    Raciocínio Lógico
+        lógica 
+            matemática
+            quantitativa
+            numerica
+            analítica
+            argumetativa
+            crítica
+            (...)
+
+'''
+
+'''
+    "as fronteiras entre a Matemática e a Lógica nunca foram demarcadas."
+    Karl Poppev (1902-1994)
+
+    "Lógica e Matemática, Matemática é Lógica"
+    Bertrand Russel (1872-1970)
+
+    Lógica é do uso de quem calcula, tanto quanto o é de quem fala.
+
+    "Ciência do Raciocínio"
+'''
