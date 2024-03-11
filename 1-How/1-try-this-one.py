86a7ee17ae478e964bf332fdcbeae3294c80d127
HernaldoMeneses <140676491+HernaldoMeneses@users.noreply.github.com>
2024-03-11 09:43:29 -0300

    Create 1-try-this-one.py

---
+++ 1-How/1-try-this-one.py
@@ -0,0 +1,18 @@
+Qt = '''
+    Na porta da minha casa passam dois ônibus, um A e outro B.
+    Um deles passa pelo Ministério da Fazenda;
+    o outro, não.
+    Na casa ao lado da minha, moram dois irmãos.
+    Um só diz a verdade, outro só diz mentira.
+    Ao indagar sobre qual ônibus tomar para chegar ao Ministério da Fazenda,
+    um dos irmãoes me disse:
+    "Se meu irmão estivesse aqui, mandaria você tomar o ônibus A".
+    Que ônibus devo tomar?
+'''
+
+WH = '''
+
+'''
+
+#8/338
+#file:///L:/Linear_/Logica/Racioc%C3%ADnio%20L%C3%B3gico-Quantitativo%20Augusto%20C.%20Morgado.pdf
